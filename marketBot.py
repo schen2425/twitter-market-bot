2d81721eabe1ee53ae312a7a034073241054dcad
Steven Chen <schen2425@gmail.com>
2020-07-13 19:49:11 -0400

    fixed timing

--- marketBot.py
+++ marketBot.py
@@ -5,6 +5,7 @@ import tweepy
 import datetime
 from marketBotHelper import postStartStock, postEndStock, postTime, isWeekday
 import finnhub
+import time
 
 # Loads the API keys from .env file
 load_dotenv()
@@ -43,7 +44,10 @@ def closePost():
     postEndStock(twitterApi, finnhub_client, trackedStocks)
 
 # Checks what time it is and runs appropate method
+# Sleeps for 65 seconds since API seems to lag by 1 min
 if ((datetime.datetime.now().hour < 12) and isWeekday()):
+    time.sleep(65)
     openPost()
 elif ((datetime.datetime.now().hour > 14) and isWeekday()):
+    time.sleep(65)
     closePost()
